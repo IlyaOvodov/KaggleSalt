c10c23ecf183fed3cd573976e77ee2a4c3fd6366
Ilya Ovodov <b@ovdv.ru>
2018-10-11 00:05:09 +0300

    Refactoring: unified model creation (train, submit)

--- MyPipeline/run_seg_test.py
+++ MyPipeline/run_seg_test.py
@@ -1,5 +1,46 @@
-
 # coding: utf-8
+
+# # Imports
+
+# In[ ]:
+
+
+import sys
+# sys.path.append(r'D:\Programming\3rd_party\keras')
+from imp import reload
+import numpy as np
+import keras
+import datetime
+import time
+
+from keras.models import Model, load_model
+from keras.layers import Input, Dropout, BatchNormalization, Activation, Add
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+from keras import backend as K
+
+import tensorflow as tf
+from distutils.version import StrictVersion
+
+sys.path.append('../3rd_party/LovaszSoftmax/tensorflow')
+import lovasz_losses_tf as L_loss
+
+import load_data
+load_data = reload(load_data)
+import keras_unet_divrikwicky_model
+keras_unet_divrikwicky_model = reload(keras_unet_divrikwicky_model)
+from my_augs import AlbuDataGenerator
+
+sys.path.append('../3rd_party/segmentation_models')
+import segmentation_models
+segmentation_models = reload(segmentation_models)
+from segmentation_models.utils import set_trainable
+
+
+
 def LoadModelParams(model_name):
     import ast
     with open(model_name + '.param.txt') as f:
@@ -21,21 +62,128 @@ def  RunTestOnSizes(params,
         params.nn_image_size = sz[2]
         RunTest(params, model_name_template=model_name_template)
 
+    # # IOU metric
+
+    # In[ ]:
+
+
+thresholds = np.array([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+
+def iou(img_true, img_pred):
+    assert (img_true.shape[-1]==1) and (len(img_true.shape)==3) or (img_true.shape[-1]!=1) and (len(img_true.shape)==2)
+    i = np.sum((img_true*img_pred) >0)
+    u = np.sum((img_true + img_pred) >0)
+    if u == 0:
+        return 1
+    return i/u
+
+def iou_metric(img_true, img_pred):
+    img_pred = img_pred > 0.5 # added by sgx 20180728
+    if img_true.sum() == img_pred.sum() == 0:
+        scores = 1
+    else:
+        scores = (thresholds <= iou(img_true, img_pred)).mean()
+    return scores
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = len(y_true_in)
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    #print("metric = ",metric)
+    return np.mean(metric)
+
+# adapter for Keras
+def my_iou_metric(label, pred):
+    metric_value = tf.py_func(iou_metric_batch, [label, pred], tf.float64)
+    return metric_value
+
+def lavazs_loss(labels, scores):  # Keras and TF has reversed order of args
+    return L_loss.lovasz_hinge(2 * scores - 1, labels, ignore=255, per_image=True)
+
+def bce_lavazs_loss(labels, scores):  # Keras and TF has reversed order of args
+    alpha = 0.1
+    return alpha*keras.losses.binary_crossentropy(labels, scores) + (1-alpha)*lavazs_loss(labels, scores)
+
+if StrictVersion(keras.__version__) < StrictVersion('2.2.3'):
+    print('Old Keras {}'.format(StrictVersion(keras.__version__)))
+    UpSampling2DLayerClass = segmentation_models.fpn.layers.UpSampling2D
+else:
+    UpSampling2DLayerClass = keras.layers.UpSampling2D
+custom_objects = {'my_iou_metric': my_iou_metric,
+                  'UpSampling2D': UpSampling2DLayerClass,
+                  'bce_lavazs_loss': bce_lavazs_loss
+                  }
+print('UpSampling2DLayerClass', UpSampling2DLayerClass)
+
+def LoadModel(model_fn):
+    model = load_model(model_fn, custom_objects=custom_objects)
+    print('MODEL LOADED from: ' + model_fn)
+    return model
+
+def CreateModel(model_fn):
+    model = None
+    if params.model == 'FNN':
+        model = segmentation_models.FPN(backbone_name=params.backbone, input_shape=(None, None, params.channels),
+                                        encoder_weights=params.initial_weightns, freeze_encoder=True,
+                                        dropout=params.dropout,
+                                        **params.model_params)
+    if params.model == 'FNNdrop':
+        model = segmentation_models.FPNdrop(backbone_name=params.backbone, input_shape=(None, None, params.channels),
+                                            encoder_weights=params.initial_weightns, freeze_encoder=True,
+                                            dropout=params.dropout,
+                                            **params.model_params)
+    if params.model == 'Unet':
+        model = segmentation_models.Unet(backbone_name=params.backbone, input_shape=(None, None, params.channels),
+                                         encoder_weights=params.initial_weightns, freeze_encoder=True,
+                                         **params.model_params)
+    if params.model == 'Linknet':
+        model = segmentation_models.Linknet(backbone_name=params.backbone, input_shape=(None, None, params.channels),
+                                            encoder_weights=params.initial_weightns, freeze_encoder=True,
+                                            **params.model_params)
+    if params.model == 'divrikwicky':
+        model = keras_unet_divrikwicky_model.CreateModel(params.nn_image_size,
+                                                         **params.model_params)
+        params.backbone = ''
+    assert model
+    return model
+
+def CompileModel(model, params):
+    for l in model.layers:
+        if isinstance(l, UpSampling2DLayerClass):
+            if hasattr(l, 'interpolation'):
+                print(l.interpolation)
+                if hasattr(params, 'model_params') and 'interpolation' in params.model_params:
+                    l.interpolation = params.model_params['interpolation']
+            else:
+                print('qq')
+    if hasattr(params, 'kernel_constraint_norm') and params.kernel_constraint_norm:
+        for l in model.layers:
+            if hasattr(l, 'kernel_constraint'):
+                print('kernel_constraint for ', l, ' is set to ',  params.kernel_constraint_norm)
+                l.kernel_constraint = keras.constraints.get(keras.constraints.max_norm(params.kernel_constraint_norm))
+    optimizer=params.optimizer
+    if optimizer == 'adam':
+        optimizer = keras.optimizers.adam(**params.optimizer_params)
+    elif optimizer == 'sgd':
+        optimizer = keras.optimizers.sgd(**params.optimizer_params)
+    if not hasattr(params, 'loss'):
+        params.loss = 'binary_crossentropy'
+    loss = params.loss
+    if loss == 'bce_lavazs_loss':
+        loss = bce_lavazs_loss
+    model.compile(loss=loss, optimizer=optimizer, metrics=["acc", my_iou_metric]) #, my_iou_metric
+
+
+
+
 def RunTest(params,
             model_name_template = 'models_3/{model}_{backbone}_{optimizer}_{augmented_image_size}-{padded_image_size}-{nn_image_size}_lrf{lrf}_{metric}_{CC}_f{test_fold_no}_{phash}'
             ):
 
     # # Params
-
-    # In[ ]:
-
     DEV_MODE_RANGE = 0 # off
-
-
-    # In[ ]:
-
-    # In[ ]:
-
 
     def params_dict():
         return {x[0]:x[1] for x in vars(params).items() if not x[0].startswith('__')}
@@ -55,77 +203,12 @@ def RunTest(params,
             if verbose:
                 print('params: '+ s + '\nsaved to ' + params_fn)
             
-
-
-    # # Imports
-
-    # In[ ]:
-
-
-    import sys
-    #sys.path.append(r'D:\Programming\3rd_party\keras')
-
-
-    # In[ ]:
-
-
-    import sys
-    from imp import reload
-    import numpy as np
-    import keras
-    import datetime
-    import time
-
-    from keras.models import Model, load_model
-    from keras.layers import Input,Dropout,BatchNormalization,Activation,Add
-    from keras.layers.core import Lambda
-    from keras.layers.convolutional import Conv2D, Conv2DTranspose
-    from keras.layers.pooling import MaxPooling2D
-    from keras.layers.merge import concatenate
-    from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
-    from keras import backend as K
-
-    import tensorflow as tf
-    sys.path.append('../3rd_party/LovaszSoftmax/tensorflow')
-    import lovasz_losses_tf as L_loss
-
-    # # Load data
-
-    # In[ ]:
-
-
-    import load_data
-    load_data = reload(load_data)
-    import keras_unet_divrikwicky_model
-    keras_unet_divrikwicky_model = reload(keras_unet_divrikwicky_model)
-
-
-    # In[ ]:
-
-
     train_df = load_data.LoadData(train_data = True, DEV_MODE_RANGE = DEV_MODE_RANGE, to_gray = False)
-
-
-    # In[ ]:
-
-
-    train_df.images[0].shape
-
-
-    # In[ ]:
-
-
     train_images, train_masks, validate_images, validate_masks = load_data.SplitTrainData(train_df, params.test_fold_no)
     train_images.shape, train_masks.shape, validate_images.shape, validate_masks.shape
 
-
     # # Reproducability setup:
-
-    # In[ ]:
-
-
     import random as rn
-
     import os
     os.environ['PYTHONHASHSEED'] = '0'
     params.seed += params.test_fold_no
@@ -138,54 +221,10 @@ def RunTest(params,
     sess = tf.Session(graph=tf.get_default_graph())
     K.set_session(sess)
 
-
-    # # IOU metric
-
-    # In[ ]:
-
-
-    thresholds = np.array([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
-
-    def iou(img_true, img_pred):
-        assert (img_true.shape[-1]==1) and (len(img_true.shape)==3) or (img_true.shape[-1]!=1) and (len(img_true.shape)==2)
-        i = np.sum((img_true*img_pred) >0)
-        u = np.sum((img_true + img_pred) >0)
-        if u == 0:
-            return 1
-        return i/u
-
-    def iou_metric(img_true, img_pred):
-        img_pred = img_pred > 0.5 # added by sgx 20180728
-        if img_true.sum() == img_pred.sum() == 0:
-            scores = 1
-        else:
-            scores = (thresholds <= iou(img_true, img_pred)).mean()
-        return scores
-
-    def iou_metric_batch(y_true_in, y_pred_in):
-        batch_size = len(y_true_in)
-        metric = []
-        for batch in range(batch_size):
-            value = iou_metric(y_true_in[batch], y_pred_in[batch])
-            metric.append(value)
-        #print("metric = ",metric)
-        return np.mean(metric)
-
-    # adapter for Keras
-    def my_iou_metric(label, pred):
-        metric_value = tf.py_func(iou_metric_batch, [label, pred], tf.float64)
-        return metric_value
-
-
     # # Data generator
-
-    # In[ ]:
-
-
     mean_val = np.mean(train_images.apply(np.mean))
     mean_std = np.mean(train_images.apply(np.std))
     mean_val, mean_std 
-
 
     #####################################
     def FillCoordConvNumpy(imgs):
@@ -219,77 +258,15 @@ def RunTest(params,
     
     ######################################
     
-    from my_augs import AlbuDataGenerator
-
-    def lavazs_loss(labels, scores):  # Keras and TF has reversed order of args
-        return L_loss.lovasz_hinge(2 * scores - 1, labels, ignore=255, per_image=True)
-
-    def bce_lavazs_loss(labels, scores):  # Keras and TF has reversed order of args
-        alpha = 0.1
-        return alpha*keras.losses.binary_crossentropy(labels, scores) + (1-alpha)*lavazs_loss(labels, scores)
-
-
     # # model
-
-    # In[ ]:
-
-
-    sys.path.append('../3rd_party/segmentation_models')
-    import segmentation_models
-    segmentation_models = reload(segmentation_models)
-    from segmentation_models.utils import set_trainable
-
-
-    # In[ ]:
     if not hasattr(params, 'model_params'):
         params.model_params = {}
 
     if params.load_model_from:
-        model = load_model(params.load_model_from,
-                           custom_objects={'my_iou_metric': my_iou_metric}
-                           )
-        print('MODEL LOADED from: ' + params.load_model_from)
+        model = LoadModel(params.load_model_from)
     else:
-        model = None
-        if params.model == 'FNN':
-            model = segmentation_models.FPN(backbone_name=params.backbone, input_shape=(None, None, params.channels),
-                                            encoder_weights=params.initial_weightns, freeze_encoder=True,
-                                            dropout = params.dropout,
-                                            **params.model_params)
-        if params.model == 'FNNdrop':
-            model = segmentation_models.FPNdrop(backbone_name=params.backbone, input_shape=(None, None, params.channels),
-                                            encoder_weights=params.initial_weightns, freeze_encoder=True,
-                                            dropout = params.dropout,
-                                            **params.model_params)
-        if params.model == 'Unet':
-            model = segmentation_models.Unet(backbone_name=params.backbone, input_shape=(None, None, params.channels),
-                                             encoder_weights=params.initial_weightns, freeze_encoder=True,
-                                            **params.model_params)
-        if params.model == 'Linknet':
-            model = segmentation_models.Linknet(backbone_name=params.backbone, input_shape=(None, None, params.channels),
-                                                encoder_weights=params.initial_weightns, freeze_encoder=True,
-                                            **params.model_params)
-        if params.model == 'divrikwicky':
-            model = keras_unet_divrikwicky_model.CreateModel(params.nn_image_size,
-                                            **params.model_params)
-            params.backbone = ''
-        assert model
-
-    for l in model.layers:
-        if isinstance(l, segmentation_models.fpn.layers.UpSampling2D) or isinstance(l, keras.layers.UpSampling2D):
-            print(l)
-            if hasattr(l, 'interpolation'):
-                print(l.interpolation)
-                if hasattr(params, 'model_params') and 'interpolation' in params.model_params:
-                    l.interpolation = params.model_params['interpolation']
-            else:
-                print('qq')
-
-    if hasattr(params, 'kernel_constraint_norm') and params.kernel_constraint_norm:
-        for l in model.layers:
-            if hasattr(l, 'kernel_constraint'):
-                print('kernel_constraint for ', l, ' is set to ',  params.kernel_constraint_norm)
-                l.kernel_constraint = keras.constraints.get(keras.constraints.max_norm(params.kernel_constraint_norm))
+        model = CreateModel(params)
+    CompileModel(model, params)
 
     # In[ ]:
     #print(model.summary())
@@ -307,33 +284,7 @@ def RunTest(params,
     params_save(model_out_file, verbose = True)
     log_out_file = model_out_file+'.log.csv'
 
-
-    # In[ ]:
-
-
-    #model = load_model(model1_file, ) #, 'lavazs_loss': lavazs_loss
-
-
     # # Train
-
-    # In[ ]:
-
-    optimizer=params.optimizer
-    if optimizer == 'adam':
-        optimizer = keras.optimizers.adam(**params.optimizer_params)
-    elif optimizer == 'sgd':
-        optimizer = keras.optimizers.sgd(**params.optimizer_params)
-
-    if not hasattr(params, 'loss'):
-        params.loss = 'binary_crossentropy'
-    loss = params.loss
-    if loss == 'bce_lavazs_loss':
-        loss = bce_lavazs_loss
-
-    model.compile(loss=loss, optimizer=optimizer, metrics=["acc", my_iou_metric]) #, my_iou_metric
-
-
-    # In[ ]:
 
     if params.coord_conv:
         mean = (np.asarray((0.,mean_val,0.)), np.asarray((1.,mean_std,1.)))
